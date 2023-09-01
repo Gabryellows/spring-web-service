e7bb794653316d2ee2c45f6a1996cd355cd40f38
Gabriel Leal <gabryellows@gmail.com>
2023-09-01 12:52:44 -0300

    H2 databse, created test profile, JPA dependency

--- src/main/java/com/webservice/course/model/entities/User.java
+++ src/main/java/com/webservice/course/model/entities/User.java
@@ -1,11 +1,16 @@
 package com.webservice.course.model.entities;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements java.io.Serializable{
 
     private static final long serialVersionUID = 1L;
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
